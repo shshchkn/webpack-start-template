3d82838a62ceaba49dd10a25b86139eaa4993efe
shshchkn <shshchkn@yandex.ru>
2017-09-23 18:55:44 +0400

    Pug done

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -4,4 +4,4 @@ let menu = createMenu(['Home', 'About', 'Contacts', 'main-menu']);
 
 document.body.appendChild(menu);
 
-console.log('In index.js');
+console.log('In blog.js');
--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,24 @@ const PATHS = {
 };
 
 module.exports = {
-    entry: PATHS.src + '/pages/index/index.js',
+    entry: {
+        'index': PATHS.src + '/pages/index/index.js',
+        'blog': PATHS.src + '/pages/blog/blog.js'
+    },
     output: {
       path: PATHS.build,
       filename: '[name].js'
     },
     plugins: [
         new HTML({
+            filename: 'index.html',
+            chunks: ['index'],
             template: PATHS.src + '/pages/index/index.pug'
+        }),
+        new HTML({
+            filename: 'blog.html',
+            chunks: ['blog'],
+            template: PATHS.src + '/pages/blog/blog.pug'
         })
     ],
     module: {
