ce61071c08d0065a49eee6feb619958acbeac7c8
shshchkn <shshchkn@yandex.ru>
2017-09-24 11:52:13 +0400

    Split config

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,50 @@
 const path = require('path');
 const HTML = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./config/pug');
+const devserver = require('./config/devserver');
 
 const PATHS = {
     src: path.join(__dirname, 'src'),
     build: path.join(__dirname, 'build')
 };
 
-const common = {
-    entry: {
-        'index': PATHS.src + '/pages/index/index.js',
-        'blog': PATHS.src + '/pages/blog/blog.js'
-    },
-    output: {
-      path: PATHS.build,
-      filename: '[name].js'
-    },
-    plugins: [
-        new HTML({
-            filename: 'index.html',
-            chunks: ['index'],
-            template: PATHS.src + '/pages/index/index.pug'
-        }),
-        new HTML({
-            filename: 'blog.html',
-            chunks: ['blog'],
-            template: PATHS.src + '/pages/blog/blog.pug'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                loader: 'pug-loader',
-                options: {
-                    pretty: true
-                }
-            }
+const common = merge(
+    {
+        entry: {
+            'index': PATHS.src + '/pages/index/index.js',
+            'blog': PATHS.src + '/pages/blog/blog.js'
+        },
+        output: {
+          path: PATHS.build,
+          filename: '[name].js'
+        },
+        plugins: [
+            new HTML({
+                filename: 'index.html',
+                chunks: ['index'],
+                template: PATHS.src + '/pages/index/index.pug'
+            }),
+            new HTML({
+                filename: 'blog.html',
+                chunks: ['blog'],
+                template: PATHS.src + '/pages/blog/blog.pug'
+            })
         ]
-    }
-};
-
-const developmentConfig = {
-    devServer: {
-        stats: 'errors-only',
-        port: 9000
-    }
-};
+    },
+    pug()
+);
 
 module.exports = (env) => {
     if (env === 'production') {
         return common;
     }
     if (env === 'development') {
-        return Object.assign({},
-            common,
-            developmentConfig
+        return merge(
+            [
+                common,
+                devserver()
+            ]
         );
     }
 };
